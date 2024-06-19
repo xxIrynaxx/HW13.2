075b966c366cd14f24e67c9daba489a10e63178f
xxIrynaxx <irakalachenko2006@gmail.com>
2024-06-19 09:44:02 +0300

    Unit tests for previous tasks

--- calculator.js
+++ calculator.js
@@ -1,27 +1,40 @@
 /* eslint-disable default-case */
 
-export const calc = expression => {
-  if (typeof expression !== 'string') {
+export const reverseArray = arr => {
+  if (!Array.isArray(arr)) {
     return null;
   }
-
-  const [a, operation, b] = expression.split(' ');
-  let result;
-
-  switch (operation) {
-    case '+':
-      result = +a + +b;
-      break;
-    case '-':
-      result = +a - +b;
-      break;
-    case '*':
-      result = +a * +b;
-      break;
-    case '/':
-      result = +a / +b;
-      break;
-  }
-
-  return `${expression} = ${result}`;
+  return arr.reverse();
 };
+
+console.log(reverseArray('Hi')); // -> null
+console.log(reverseArray(['H', 'e', 'y'])); // -> ['y', 'e', 'H']
+console.log(reverseArray([3, 8, 5, 0, 4, 2])); // -> [2, 4, 0, 5, 8, 3]
+
+export const withdraw = (clients, balances, client, amount) => {
+  const user = clients.indexOf(client);
+  const balance = balances[user];
+  if (balance > amount) {
+    return balance - amount;
+  } else {
+    return -1;
+  }
+};
+
+console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50)); // ==> 37 (balances array should be [1400, 37, -6])
+console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'User', 10)); // ==> -1 (balances array should be [1400, 87, -6])
+console.log(withdraw(['Iryna', 'Daria', 'Max'], [1400, 87, -6], 'Iryna', 300)); // ==> 1100
+
+export const getAdults = obj => {
+  const newObj = {};
+  for (let keys in obj) {
+    if (obj[keys] >= 18) {
+      newObj[keys] = obj[keys];
+    }
+  }
+  return newObj;
+};
+
+console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })); // ==> { 'John Doe': 19, Bob: 18 }
+console.log(getAdults({ Ann: 56, Andrey: 7 })); // ==> { Ann: 56 }
+console.log(getAdults({ Iryna: 18, Daria: 15 }));
--- calculator.test.js
+++ calculator.test.js
@@ -1,26 +1,51 @@
-import { calc } from './calculator';
+import { reverseArray, withdraw, getAdults } from './calculator';
 
-it('should return null', () => {
-  const result = calc(1 + 5);
+it('should get reverse array', () => {
+  const result = reverseArray('Hi');
   expect(result).toEqual(null);
 });
 
-it('should return sum of expression', () => {
-  const result = calc('1 + 4');
-  expect(result).toEqual('1 + 4 = 5');
+it('should get reverse array', () => {
+  const result = reverseArray(['H', 'e', 'y']);
+  expect(result).toEqual(['y', 'e', 'H']);
 });
 
-it('should return substraction of expression', () => {
-  const result = calc('1 - 4');
-  expect(result).toEqual('1 - 4 = -3');
+it('should get reverse array', () => {
+  const result = reverseArray([3, 8, 5, 0, 4, 2]);
+  expect(result).toEqual([2, 4, 0, 5, 8, 3]);
 });
 
-it('should return multiply of expression', () => {
-  const result = calc('1 * 4');
-  expect(result).toEqual('1 * 4 = 4');
+it("should receive the client's account balance", () => {
+  const result = withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50);
+  expect(result).toEqual(37);
 });
 
-it('should return division of expression', () => {
-  const result = calc('1 / 4');
-  expect(result).toEqual('1 / 4 = 0.25');
+it("should receive the client's account balance", () => {
+  const result = withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'User', 10);
+  expect(result).toEqual(-1);
+});
+
+it("should receive the client's account balance", () => {
+  const result = withdraw(
+    ['Iryna', 'Daria', 'Max'],
+    [1400, 87, -6],
+    'Iryna',
+    300
+  );
+  expect(result).toEqual(1100);
+});
+
+it('should get users 18 yers or older', () => {
+  const result = getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 });
+  expect(result).toEqual({ 'John Doe': 19, Bob: 18 });
+});
+
+it('should get users 18 yers or older', () => {
+  const result = getAdults({ Ann: 56, Andrey: 7 });
+  expect(result).toEqual({ Ann: 56 });
+});
+
+it('should get users 18 yers or older', () => {
+  const result = getAdults({ Iryna: 18, Daria: 15 });
+  expect(result).toEqual({ Iryna: 18 });
 });
